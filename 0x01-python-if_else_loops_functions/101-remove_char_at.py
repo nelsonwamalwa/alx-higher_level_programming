5c1a45cbb38f2c1b8dc8f17e3e93739543345146
NelsonWamalwa <wamalwanelson@gmail.com>
2023-09-05 23:57:40 +0300

    remove char  trial

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 101-remove_char_at.py
+
+
+def remove_char_at(str, n):
+    """Creates a copy of string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
