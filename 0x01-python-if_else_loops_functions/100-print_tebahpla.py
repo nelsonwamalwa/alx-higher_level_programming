ddc47ad8efd81c80ead6f78375801421fdec4f14
NelsonWamalwa <wamalwanelson@gmail.com>
2023-09-05 23:49:35 +0300

    100-print_tebahpla.py trial

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,7 @@
-for char in range(ord('z'), ord('A') - 1, -1):
-    print(chr(char), end='')
+#!/usr/bin/python3
+
+""""Prints the alphabet in reverse order alternating the upper- and the lower-cases."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
