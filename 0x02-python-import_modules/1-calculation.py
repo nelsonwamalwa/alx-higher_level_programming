967a777d7a275823207df433eb516d47989525df
NelsonWamalwa <wamalwanelson@gmail.com>
2023-09-07 20:24:01 +0300

    Mandatory tasks test

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Printing the sum multiple,quotient and difference of 5 and 10 """
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Printing the value of the variable a from the variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
