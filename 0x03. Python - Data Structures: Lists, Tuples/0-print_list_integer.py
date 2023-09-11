67f5ee89bb20f98b0b738cd7846110a50496b184
NelsonWamalwa <wamalwanelson@gmail.com>
2023-09-11 18:34:39 +0300

    Unfinished project

---
+++ 0x03. Python - Data Structures: Lists, Tuples/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """We are printing all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
---
+++ 0x03. Python - Data Structures: Lists, Tuples/README.md
@@ -0,0 +1,4 @@
+## Python - Data Structures: Lists, Tuples
+
+Just learned about how the sequence data types work in
+Python - specifically, lists and tuples.
