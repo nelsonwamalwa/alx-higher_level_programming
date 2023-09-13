74fcc96fb045d53396d88770f78d0b20c72ccbe8
NelsonWamalwa <wamalwanelson@gmail.com>
2023-09-13 16:47:26 +0300

    Unscripted files

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replacing or adding the key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
