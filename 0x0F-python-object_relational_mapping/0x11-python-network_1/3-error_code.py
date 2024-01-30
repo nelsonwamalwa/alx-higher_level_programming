66c7665fc7d2d18f27871210752ccbe190276a86
NelsonWamalwa <wamalwanelson@gmail.com>
2024-01-31 01:58:30 +0300

    Add new Python scripts for 0x11-python-network_1

---
+++ 0x0F-python-object_relational_mapping/0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sending request to a given URL and displaying the response body
+Usage: ./3-error_code.py <URL>
+  - Handling HTTP errors
+"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
